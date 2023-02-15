c9b09f6e68ce7f5d49be5041750e3913969e4de9
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-15 10:31:14 -0800

    Auto

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ long int longintType;
 long long int longlongintType;
 
  
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long int: %zu bytes\n", sizeof(longType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+printf("Size of char: %zu byte\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of long int: %zu bytes\n", sizeof(long));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongint));
  
 return 0;
 }
