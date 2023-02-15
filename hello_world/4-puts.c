8ba460dbda0c14b6ab7b00a79780ce655b2805ab
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-15 09:36:19 -0800

    Auto

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #!/bin/bash
 #include<stdio.h>
+
 /**
  * main - Use the function puts
  * Return: always 0
  */
+ 
 int main(void)
 {
 char gise[] = "\"Programming is like building a multilingual puzzle";
+
 puts(alow);
+
 return (0);
 }
