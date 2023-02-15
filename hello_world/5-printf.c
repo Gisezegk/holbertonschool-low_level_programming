657039394cecaba8c2fa3c7748716e362a39917e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-15 09:49:36 -0800

    Auto

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char impres[] = "\"with proper grammar, but the outcome is a piece of art,";
+char impres[] = "with proper grammar, but the outcome is a piece of art/,";
 
 puts(impres);
 
