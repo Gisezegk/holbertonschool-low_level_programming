d2e734e5c710bb0701d0c8fec3c8a385c307e595
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-15 09:44:12 -0800

    Auto

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Use the function printf
+ * Return: always 0
+ */
+
+int main(void)
+{
+char impres[] = "\"with proper grammar, but the outcome is a piece of art,";
+
+puts(impres);
+
+return (0);
+}
