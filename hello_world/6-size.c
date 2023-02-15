10ccb07b9a69470d1ab3a7757ce67314a28353a8
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-15 10:43:14 -0800

    Auto

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/**
+ * main - compiled
+ * Return: always 0
+ */
+
 int main(void)
 {
 
@@ -8,11 +13,11 @@ int intType;
 long int longintType;
 long long int longlongintType;
 float floatType;
- 
+
 printf("Size of char: %zu byte\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long));
 printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-printf("Size of float: %zu bytes\n", sizeof(float)); 
-return 0;
+printf("Size of float: %zu bytes\n", sizeof(float));
+return (0);
 }
