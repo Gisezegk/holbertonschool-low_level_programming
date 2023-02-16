029b22f70ca0ef64b8ba629564966e4de0674cf9
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-16 10:44:46 -0800

    auto

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,16 +23,15 @@ int main(void)
 	printf("Last digit of %d is %d and is greater than 5", n, digit);
 	}
 
-	else if (n == 0)
-	{
-
-		printf("Last digit of %d is %d and is 0", n, digit);
-	}
-
 	else if (n < 6 && n != 0)
 	{
 
 		printf("Last digit of %d is %d and is less than 6 and not 0 ", n, digit);
 	}
+	else (n == 0)
+        {
+
+                printf("Last digit of %d is %d and is 0", n, digit);
+        }
 	return (0);
 }
