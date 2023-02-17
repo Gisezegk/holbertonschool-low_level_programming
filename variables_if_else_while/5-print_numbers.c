f03978367d53c00a653f198ddab4dcc4d2c18e89
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-17 07:44:29 -0800

    auto

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - compiled
+* Return: always 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
