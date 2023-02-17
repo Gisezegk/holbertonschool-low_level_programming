6d869192e2a2bc72955d0e180bd6967eef120b60
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-17 05:20:33 -0800

    auto

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - compiled
+* Return: always 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	return (0);
+}
