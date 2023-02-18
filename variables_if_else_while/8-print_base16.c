fa337bb907529760b8603de314b074dd8a812d15
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-18 14:48:48 -0800

    auto

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - compiled
+* Return: always 0
+*/
+
+
+
+int main(void)
+{
+	char ch = 48;
+
+	for (ch = 48; ch <= 57; ch++)
+	putchar (ch);
+
+	for (ch = 97; ch <= 102; ch++)
+	putchar (ch);
+	putchar ('\n');
+	return (0);
+}
