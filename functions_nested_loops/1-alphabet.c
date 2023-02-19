17b0d15ae0499931db2ba5ce945e90a5ff84eea8
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-18 17:31:07 -0800

    auto

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - compiled
+* Return: always 0
+*/
+
+int main(void)
+{
+
+	char alph = 'a';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar (alph);
+}
+putchar ('\n');
+return (0);
+}
+
