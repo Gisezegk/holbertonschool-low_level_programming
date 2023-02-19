910ce9bebd9e514c630968c870bb3f206cc87bdb
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-18 20:52:53 -0800

    auto

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,9 +8,11 @@
 
 int _isalpha(int c)
 {
-if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+
+	if
+		((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
-		return (1);
+			return (1);
 	}
 	else
 	{
