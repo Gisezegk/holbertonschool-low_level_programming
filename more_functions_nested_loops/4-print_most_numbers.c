d47a310e32e0d3cc72ed885f2f7d4f80edae278c
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 07:15:21 -0800

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* 
+* Return: always 0
+*/
+
+int main(void)
+{
+	int ch = 0;
+
+	for (ch = '0'; ch <= '9'; ch++)
+
+	if (a != 2 && a != 4)
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
