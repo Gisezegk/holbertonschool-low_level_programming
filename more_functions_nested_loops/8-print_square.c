6fa060119a23b635f6e62c1c396eb3d06bd789ca
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 09:27:20 -0800

    8squares

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_square - prints the alphabet.
+*@size: square size
+*/
+
+void print_square(int size)
+{
+	int pr;
+		int rm;
+if (size > 0)
+{
+
+		for (pr = 0; pr < size; pr++)
+		{
+			for (rm = 0; rm < size; rm++)
+			{
+				_putchar(35);
+			}
+				_putchar('\n');
+		}
+}
+else
+{
+	_putchar('\n');
+}
+}
