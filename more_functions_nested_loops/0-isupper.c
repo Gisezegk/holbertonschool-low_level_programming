1f80b92deda474e405ba39e51860b4ccbd884612
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 05:01:37 -0800

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+* _isuppercase - is uppercase or no.
+* @c: variable for ch
+* Return: 0
+*/
+
+int _isuppercase(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+
+}
+}
