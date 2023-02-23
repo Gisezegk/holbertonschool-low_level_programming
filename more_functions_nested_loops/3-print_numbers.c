1b3b38389c0fd3859b61ccbf2977a3c75fd29feb
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 06:20:43 -0800

    2-mul

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the secuence
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+char num = '0';
+
+	for (num = '0'; num <= '9'; num++)
+	_putchar(num);
+
+
+
+	_putchar('\n');
+}
