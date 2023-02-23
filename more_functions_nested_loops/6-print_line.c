3c0120958d1611935b4797523fbf9f8995470d8c
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 09:01:35 -0800

    6print-line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_line - prints the alphabet.
+*
+* @n: variable
+*/
+
+void print_line(int n)
+{
+	int c = 0;
+
+	for (c = 0; c < n; c++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
