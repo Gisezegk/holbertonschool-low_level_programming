6cae132a605d334344f0c626970c1be567d0ad1f
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 05:51:00 -0800

    2mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two integers.
+ *@a: variable for numer
+ *@b: variable for number
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	printf("%d\n", (a) * (b));
+	return (0);
+}
