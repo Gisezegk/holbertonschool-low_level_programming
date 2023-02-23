a5e84a046a10934353c34fdd943b78e95ea31ed3
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-23 07:54:45 -0800

    4-print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
-#include <time.h>
+#include "main.h"
 #include <stdio.h>
 /**
-* 
+* print_most_numbers - prints numbers.
 * Return: always 0
 */
 
-int main(void)
+void print_most_numbers(void)
 {
 	int ch = 0;
 
 	for (ch = '0'; ch <= '9'; ch++)
-
-	if (a != 2 && a != 4)
+	{
+	if (ch != 2 && ch != 4)
 	_putchar(ch);
+	}
 	_putchar('\n');
-	return (0);
 }
