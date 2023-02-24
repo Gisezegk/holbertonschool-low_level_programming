3b1338b5df670213a9a131eb73baec36cd0fd613
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-24 10:23:37 -0800

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_triangle - patric
+* @size: size
+* Return: Always 0
+*/
+
+void print_triangle(int size)
+{
+        int c;
+	int f;
+
+        if (size > 0)
+        {
+        for (c = 0; c < size; c++)
+        {
+                for (f = 0; f < size; f++)
+		{
+		if (f < size - c - 1)
+			{
+
+		_putchar(' ');
+			}
+
+		else
+			{
+				_putchar(35);
+				
+			}
+		}
+		_putchar('\n');
+	}
+	}
+		else {
+			_putchar(10);
+	}
+}
