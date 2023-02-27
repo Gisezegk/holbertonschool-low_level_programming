a87e4492a627cee36aabb52b926ff05fd80c9ae4
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-27 07:33:00 -0800

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - Updates the value it points to 98. 
+ * reset_to_98 - Updates the value it points to 98.
  * @*n: value of variable
- * 
+ *
  */
 void reset_to_98(int *n)
 {
