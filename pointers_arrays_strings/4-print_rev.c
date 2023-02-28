52737677e1871d56fd1a092265e806d1857c9e1e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-02-28 06:37:13 -0800

    4-printrev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - print a string reverse
+* Return: Always 0
+* @s: value
+*/
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (*s++)
+	{
+		a--;
+	}
+	while (a != 1)
+	{
+		s--;
+	_putchar (*s);
+	a++;
+	}
+printf("\n");
+
+}
