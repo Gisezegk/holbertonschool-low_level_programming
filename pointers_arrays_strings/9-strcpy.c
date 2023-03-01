9b99ceeba52d929193d76b066d0bc061c035c47e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-01 11:28:37 -0800

    9strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ **_strcpy - copies the string pointed to by src
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+{
+
+	strcpy(dest, src);
+	printf("%s", dest);
+
+}
+
+return (0);
+}
+
