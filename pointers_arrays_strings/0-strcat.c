c9fcc580c04dc78f79f03e20addd58147ba90096
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-02 10:00:00 -0800

    0-strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,27 +4,26 @@
 
 
 /**
- *
- *
+ *_strcat - concatenates two strings.
+ *Return: Always dest
+ *@dest: value
+ *@src: value
  */
 
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int O = 0;
+	char *q;
+	char *p;
 
-	while(src[i] != '\0')
-	{
-		i++;
-	}
-	for (O = 0; O <= i; O++)
-	{
-		dest[O] = src[O];
-	}
-	if (O == i + 1)
-	{
-		dest[O] = '\0';
-	}
+	for (q = dest; *q != '\0'; q++)
+		;
+
+	for (p = src; *p != '\0'; p++, q++)
+		*q = *p
+			;
+
+	*q = '\0';
+
 	return (dest);
 }
---
+++ pointers_arrays_strings/test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char str[] = "12j";
+    int num = atoi(str);
+    printf("The integer value is: %d\n", num);
+    return 0;
+}
+
