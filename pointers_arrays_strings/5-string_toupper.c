27e6dd63ad097f297fc3ca2ec9bcb9546fe9a83b
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-03 09:25:09 -0800

    5-string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,18 +4,18 @@
 /**
  *string_toupper - changes all lowercase letters of a string to uppercase.
  *Return: Always 0
- *
+ *@str: value
  */
 
 char *string_toupper(char *str)
 {
 	int j = 0;
 
-	while (str[j] != '\0') 
+	while (str[j] != '\0')
 	{
 		if (str[j] >= 'a' && str[j] <= 'z')
 		{
-			str[j] = (int)str[j] -32;
+			str[j] = (int)str[j] - 32;
 		}
 		j++;
 	}
