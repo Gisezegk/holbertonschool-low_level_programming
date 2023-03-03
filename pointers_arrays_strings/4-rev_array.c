69f967031409403164abec58b16ae915c462251b
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-03 09:01:09 -0800

    4-rev_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reverse_array - reverse the content of an array of integers 
+ *Return: Always 0
+ *@a: value
+ *@n: value
+ */
+
+void reverse_array(int *a, int n)
+{
+	int at = 0, i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		at = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = at;
+	}
+}
