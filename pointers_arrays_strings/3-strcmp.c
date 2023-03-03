7418d733b94c518f1fbf7b2cfb035da15b231ec1
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-03 06:58:01 -0800

    3strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,20 +13,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, g;
 
-	while(s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	if(s1[i] == s2[i])
+	if (s1[i] == s2[i])
 	{
 		g = 0;
 	}
 
-	else if(s1[i] > s2[i])
+	else if (s1[i] > s2[i])
 	{
-		g = 15;
+		g = 13;
 	}
 	else if (s1[i] < s2[i])
 	{
-		g = -15;
+		g = -13;
 	}
 	i++;
 }
