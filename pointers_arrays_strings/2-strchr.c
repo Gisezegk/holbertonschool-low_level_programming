863c7dd6157212aab1e21cb82ca56228d1296265
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-04 20:08:43 -0800

    2strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strchr - locates a character in a string
+ *@s: value
+ *@c: value
+ *Return: Always 0;
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c) return (char *) s;
+			s++;
+		}	
+
+	return (NULL);
+}
