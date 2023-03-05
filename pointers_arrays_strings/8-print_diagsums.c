e2a0ac812a02a27b2db8c67ea5f67ce32d740c7c
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-05 08:38:25 -0800

    8-print_diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,10 +11,11 @@
 void print_diagsums(int *a, int size)
 {
 	int i, n, suma1 = 0, suma2 = 0;
+
 	for (i = 0; i <= (size * size); i = i + size + 1)
 		suma1 = suma1 + a[i];
 
-	for (n = size - 1; n <= (size * size) - size; n = n + size -1)
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 		suma2 = suma2 + a[n];
 	printf("%d, %d\n", suma1, suma2);
 }
