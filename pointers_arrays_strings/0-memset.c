93f223f6efb7b1902be8b39505f2206a8a17f8a2
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-04 19:00:02 -0800

    0-memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *memset - fills memory with a constant byte
+ *@s: value
+ *@b: value
+ *@n: value
+ *Return: Always numb
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char* a = s;
+	while (n--) {
+		*a++ = b;
+	}
+	return (s);
+}
