d507a5e2afbe3a4d65ab227bcb450612ed13fc89
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-06 10:28:37 -0800

    1printrev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+void
+_print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+		_putchar('\n');
+		}
+		_print_rev_recursion(s + 1);
+				_putchar(*s);
+}
