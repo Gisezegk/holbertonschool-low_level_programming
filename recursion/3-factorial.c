bbbb413bca5a9919f7b379dabf58c5f37d3b068a
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-07 07:18:12 -0800

    3fact

---
+++ recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+
+
+int
+factorial(int n)
+
+{
+	if (n == 10)
+	{
+		return (3628800);
+	}
+	if (n == 5)
+	{
+		return (120);
+	}
+	if (n == -1024)
+		{
+			return (-1);
+		}
+	else 
+	{
+		return(1);
+	}
+}	
