196313edaab7d9c15742fd3e9f721eb7977a3a9b
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-08 09:51:16 -0800

    5squirt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 /**
  *_sqrt_recursion - nashe
+ *@n: value
+ *Return: Always tpm
  */
 
 int _sqrt_recursion(int n)
 {
 
-        return (_sqrt_nashe(n, 1));
+	return (_sqrt_nashe(n, 1));
 }
 int
 _sqrt_nashe(int you, int tube)
@@ -15,9 +17,9 @@ _sqrt_nashe(int you, int tube)
 	int unit = tube * tube;
 
 if (unit > you)
-                return (-1);
+	return (-1);
 	if (unit == you)
-		 return (tube);
+		return (tube);
 
-	 return (_sqrt_nashe(you, tube + 1));
+	return (_sqrt_nashe(you, tube + 1));
 }
