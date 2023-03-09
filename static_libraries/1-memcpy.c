0b19836b7585b013a709db09a06b2f393c6d9a24
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-09 11:00:21 -0800

    statics

--- static_libraries/1-memcpy.c
+++ static_libraries/1-memcpy.c
@@ -1,2 +1,22 @@
-char
-*_memcpy(char *dest, char *src, unsigned int n);
+#include "main.h"
+
+/**
+ *  * _memcpy - epico
+ *   * @dest: var
+ *    * @src: var
+ *     * @n: tac
+ *      * Return: always var
+ *       *
+ *        */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+		unsigned int pos;
+
+			for (pos = 0; pos < n; pos++)
+					{
+								dest[pos] = src[pos];
+									}
+				return (dest);
+}
