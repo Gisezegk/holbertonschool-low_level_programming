c844ed6a61455a3880f0481a0a555f433dcb8669
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-09 11:05:47 -0800

    st

--- static_libraries/3-islower.c
+++ static_libraries/3-islower.c
@@ -1,1 +1,20 @@
-int _islower(int c);
+#include"main.h"
+
+/**
+ * * _islower - is lower or no.
+ * * @c: variable for ch
+ * * Return: 0
+ * */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+			return (1);
+	}
+	else
+	{
+			return (0);
+
+	}
+}
--- static_libraries/4-isalpha.c
+++ static_libraries/4-isalpha.c
@@ -1,1 +1,19 @@
-int _isalpha(int c);
+#include"main.h"
+
+/**
+ * * _isalpha - is alpha or no.
+ * * @c: variable for ch
+ * * Return: 0
+ * */
+
+int _isalpha(int c)
+{
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+				{
+							return (1);
+								}
+			else
+					{
+								return (0);
+									}
+}
