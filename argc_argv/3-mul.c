795d2057a4d3ec2cc8a04a2c4599965a3ff46037
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-10 10:38:37 -0800

    mlti

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char** argv)
+{
+	int a, b;
+	int mul = 0;
+
+	if ((argc =! 3))
+	{
+		printf("0");
+		return(0);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	mul = a * b;
+
+	printf("%d\n", mul);
+	return (0);
+}
