a868ce948c5273cc62498e01f4c8b48cb1617983
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-10 08:57:19 -0800

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+		return (0);
+}
+~                                                                                                                                                                                                                                            ~                
