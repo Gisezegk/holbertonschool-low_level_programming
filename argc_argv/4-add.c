66b03cf94da74ebbf3e325ed9e0a02574bb12777
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-10 10:59:00 -0800

    suma

---
+++ argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char* argv[])
+{
+	int i;
+	int sum;
+
+	if(argc > 0)
+	
+	{
+for (i = 1; i < argc; i++)
+{
+	:x
+
+	sum += atoi(argv[i]);
+		}
+
+}
+	i = atoi(argv[i]);
+
+	printf("%d\n", sum);
+return (0);
+}
