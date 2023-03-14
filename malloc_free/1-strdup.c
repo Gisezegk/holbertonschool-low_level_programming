180d4fe207a81a539720202b68dac33f9787d013
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-14 06:56:54 -0700

    duplicated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char* _strdup(char* str)
+{
+	size_t len;
+	char *new_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = strlen(str) + 1;
+	new_str = (char*) malloc(len * sizeof(char));
+
+		if (new_str == NULL)
+		{
+			return (NULL);
+		}
+
+		memcpy(new_str, str, len);
+
+		return new_str;
+}
