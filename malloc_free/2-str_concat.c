c3e17a9c9bf6215fcbe5fb6f09fed5c95c385a04
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-14 08:05:26 -0700

    concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+char
+*str_concat(char *s1, char *s2)
+{
+	char *content;
+	size_t len1;
+	size_t len2;
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	content = malloc(len1 + len2 + 1);
+
+	if (content == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(content, s1, len1);
+	memcpy(content + len1, s2, len2);
+	content[len1 + len2] = '\0';
+
+	return (content);
+}
