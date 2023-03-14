30c26f3953486fec66b8fd4cab86f89311281c84
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-14 05:55:54 -0700

    createarray

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ *create_array - create array
+ *@size: value
+ *@c: value
+ *Return: Always Arr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
