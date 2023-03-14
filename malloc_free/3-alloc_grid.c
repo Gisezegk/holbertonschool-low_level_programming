65c855fbd4c14ac5c8182d7122b188ee0ba42882
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-14 10:45:27 -0700

    allocgrid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+int
+**alloc_grid(int width, int height)
+{
+	int i;
+	int **init;
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+
+	init = malloc(width * height * sizeof(int));
+	
+
+if (init == NULL)
+{
+	return (NULL);
+}
+
+	for (i = 0; i < height; i++)
+	{
+	init[i] = malloc(width * sizeof(int));
+	}
+return (init);
+}
