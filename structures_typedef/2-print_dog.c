3e67f84ce2af8f3920b6b83df82cf1921653adde
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-23 07:43:44 -0700

    print

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+void
+print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
