4d39d6e32968356a755c0344cd784e8983657b6e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-25 02:15:11 -0700

    interatorfix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,7 +5,8 @@ void
 array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < size && array && action && size; i++)
 	{
 		(*action)(array[i]);
 	}
