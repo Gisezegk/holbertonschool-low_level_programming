823b08eed783b8bcc97881351b2fd3363834ebd9
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-03-26 18:02:34 -0700

    separator

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,10 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_strings(const char *separator, const unsigned int n, ...)
+void
+print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
