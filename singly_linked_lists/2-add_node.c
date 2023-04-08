b317fdcdfe6a9f8f3eb498dc66a109a9ae9f7153
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-04-08 00:27:17 -0700

    y los elementos?

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	   list_t *new_node;
+
+	   new_node = malloc(sizeof(list_t));
+
+	   if (!head || !new_node)
+	   {
+		   return(NULL);
+	   }
+
+	   new_node->str = strdup(str);
+	   new_node->next = *head;
+	   new_node->len = strlen(str);
+
+	   *head = new_node;
+
+	   return (*head);
+}
