e906225ed154d64dc25d0797ab1b2c61da099f0d
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-04-10 12:53:05 -0700

    function that adds a new node at the beginning of a double linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+dlistint_t
+*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	return (new_node);
+}
