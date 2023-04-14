a05cbc843454c8182b6b7bc3ea28b63753dd1a39
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-04-14 00:20:29 -0700

    create a function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int
+create_file(const char *filename, char *text_content)
+{
+	int fd, wbytes;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+		{
+			wbytes = write(fd, text_content, strlen(text_content));
+			if (wbytes == -1)
+			{
+				close(fd);
+				return (-1);
+			}
+		}
+	close(fd);
+	return (1);
+}
+
