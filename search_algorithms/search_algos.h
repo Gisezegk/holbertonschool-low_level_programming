f2eb59d16906c4a8492a2699eb829bdf8fb81543
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-05-26 23:11:19 -0700

    task 0

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef HOLA
+#define HOLA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
